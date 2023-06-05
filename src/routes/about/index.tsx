ab6c1fb94feedc863ed56cc0274c71950ba80a56
Mohamed Mehdawy <mohammedmehdawy@gmail.com>
2023-06-05 21:25:59 +0300

    work with route

---
+++ src/routes/about/index.tsx
@@ -0,0 +1,34 @@
+import { component$ } from "@builder.io/qwik";
+
+export default component$(() => {
+   console.log('hello')
+   return (
+      <section>
+         <h2>About page</h2>
+         <p>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Hic
+            aperiam rem voluptatem, porro, consequatur accusantium recusandae
+            enim tempora explicabo natus nisi ut nostrum molestias rerum
+            corrupti. Corporis, numquam architecto? Fugit.
+         </p>
+         <p>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Hic
+            aperiam rem voluptatem, porro, consequatur accusantium recusandae
+            enim tempora explicabo natus nisi ut nostrum molestias rerum
+            corrupti. Corporis, numquam architecto? Fugit.
+         </p>
+         <p>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Hic
+            aperiam rem voluptatem, porro, consequatur accusantium recusandae
+            enim tempora explicabo natus nisi ut nostrum molestias rerum
+            corrupti. Corporis, numquam architecto? Fugit.
+         </p>
+         <p>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Hic
+            aperiam rem voluptatem, porro, consequatur accusantium recusandae
+            enim tempora explicabo natus nisi ut nostrum molestias rerum
+            corrupti. Corporis, numquam architecto? Fugit.
+         </p>
+      </section>
+   );
+});
---
+++ src/routes/contact/index.tsx
@@ -0,0 +1,15 @@
+import { component$ } from "@builder.io/qwik";
+
+export default component$(() => {
+   return (
+      <section>
+         <h2>contact page</h2>
+         <p>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Hic
+            aperiam rem voluptatem, porro, consequatur accusantium recusandae
+            enim tempora explicabo natus nisi ut nostrum molestias rerum
+            corrupti. Corporis, numquam architecto? Fugit.
+         </p>
+      </section>
+   );
+});
