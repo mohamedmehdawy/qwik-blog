9c7f0de5a8d1b0e6814bf3e30567a451179b8086
Mohamed Mehdawy <mohammedmehdawy@gmail.com>
2023-06-06 22:55:25 +0300

    work with links and layout

---
+++ src/routes/layout-bro.tsx
@@ -0,0 +1,24 @@
+import { component$, Slot, useStyles$ } from '@builder.io/qwik';
+import { routeLoader$ } from '@builder.io/qwik-city';
+
+import Header from '~/components/starter/header/header';
+import Footer from '~/components/starter/footer/footer';
+
+// import styles from './styles.css?inline';
+
+export const useServerTimeLoader = routeLoader$(() => {
+  return {
+    date: new Date().toISOString(),
+  };
+});
+
+export default component$(() => {
+  return (
+    <>
+    <h1>new layout</h1>
+      <main>
+        <Slot />
+      </main>
+    </>
+  );
+});
